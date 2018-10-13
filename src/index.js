aafe901648f40d8b858d99b18f7e38f664d3f395
kotli <myepicmail@gmail.com>
2018-10-13 15:54:26 +0300

    f: adding support for testing and http calls

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import './index.css';
+import {getUsers, deleteUser} from './api/userApi';
 
-import numeral from 'numeral';
+// Populate table of users via API call.
+getUsers().then(result => {
+  let usersBody = "";
 
-const cValue = numeral(1000).format('$0,0.00');
+  result.forEach(user => {
+    usersBody+= `<tr>
+      <td><a href="#" data-id="${user.id}" class="deleteUser">Delete
+      <td>${user.id}</td>
+      <td>${user.firstName}</td>
+      <td>${user.lastName}</td>
+      <td>${user.email}</td>
+      </tr>`
+  });
+
+  global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from a DOM collection
+  // getElementsByClassname only returns an "array like" object
+  Array.from(deleteLinks, link => {
+    link.onclick = function(event) {
+      const element = event.target;
+      event.preventDefault();
+      deleteUser(element.attributes["data-id"].value);
+      const row = element.parentNode.parentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
+});
+
 //next line is for debuging webpack via source map
 //debugger;
-console.log(`Here is ${cValue} dollers`);// eslint-disable-line no-console
-
-
